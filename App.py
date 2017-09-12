72ab4522f3eae3bdd0c366c0aeaca38e4f12540f
David Siegel <david.siegel3@marist.edu>
2017-09-12 11:12:07 -0400

    Added pressEnter() to instruct user to press Enter to continue

--- App.py
+++ App.py
@@ -7,21 +7,17 @@ playerName = input("What is your name?")
 print("Hello " + playerName + "! Welcome to Five Nights at Freddies!")
 playerLocation = "You are outside an abandoned house, late on Halloween night. You went there looking for your friends who ran off ahead of you."
 print(playerLocation)
-def giveInstruction():{
-    print("Please press enter")
-    }
-giveInstruction()
-print("1. Shout your friends's names.")
-x = int(input("?"))
-print("=================================================")
 
-if (x == 1):
-    print("You shout out 'Jonathan!!'")
-    print("Andrew!!")
-    print("'Where are you??'")
-    print("...")
-    print("...")
-    print("...")
-    print("You get no response...")
-else:
-    print("I'm sorry that's not an option.")
+def pressEnter():
+    input("Press Enter to continue")
+pressEnter()
+print("=================================================")
+print("You shout your friends's names.")
+print("You shout out 'Jonathan!!'")
+print("Andrew!!")
+print("'Where are you??'")
+print("...")
+print("...")
+print("...")
+print("You get no response...")
+
