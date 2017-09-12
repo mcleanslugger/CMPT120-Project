2c947e1819cdb7887ceefd64ab8aea940342204a
David Siegel <david.siegel3@marist.edu>
2017-09-12 11:28:00 -0400

    Added main() function. Reformatted pressEnter() function. Added more detail to the second location.

--- App.py
+++ App.py
@@ -2,22 +2,33 @@
 ##Author: David Siegel
 ##Version: 0.1
 
-import math
-playerName = input("What is your name?")
-print("Hello " + playerName + "! Welcome to Five Nights at Freddies!")
-playerLocation = "You are outside an abandoned house, late on Halloween night. You went there looking for your friends who ran off ahead of you."
-print(playerLocation)
+def main():
+    playerName = input("What is your name?")
+    print("Hello " + playerName + "! Welcome to Five Nights at Freddies!")
+    playerLocation = "You are outside an abandoned house, late on Halloween night. You went there looking for your friends who ran off ahead of you."
+    print(playerLocation)
+    
+    def pressEnter():
+        input("Press Enter to continue")
+        print("=================================================")
+        print("")
+    pressEnter()
+    
+    print("You shout your friends's names.")
+    print("You shout out 'Jonathan!!'")
+    print("'Andrew!!'")
+    print("'Where are you??'")
+    print("...")
+    print("...")
+    print("You get no response...")
+    print("You move to the front door and try to open it...")
+    print("The door opens and you walk into a dark, dingy main foyer. You see stairs leading upstairs to your left.")
+    print("Around the corner to your right, you see stairs leading down to the basement.")
+    pressEnter()
 
-def pressEnter():
-    input("Press Enter to continue")
-pressEnter()
-print("=================================================")
-print("You shout your friends's names.")
-print("You shout out 'Jonathan!!'")
-print("Andrew!!")
-print("'Where are you??'")
-print("...")
-print("...")
-print("...")
-print("You get no response...")
+    
 
+
+
+
+main()
